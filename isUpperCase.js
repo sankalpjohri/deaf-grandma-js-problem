a08373c6f59a6395ef2b954cc67c4a95424a323a
Sankalp Johri <sankalp.johri@iiitb.org>
2018-07-08 20:31:36 +0530

    Added uppercase and get random number functions

--- isUpperCase.js
+++ isUpperCase.js
@@ -7,6 +7,19 @@
  * @param {string} s The string to be checked
  * @returns {boolean}
  */
-const isUppercase = (s) => {};
+const isUppercase = (s) => {
+  var i = 0;
+  var character = '';
+  var result = true;
+  while (i <= s.length) {
+    character = s.charAt(i);
+    if (!isNaN(character * 1)) {
+      if (character == character.toLowerCase()) {
+        result = false;
+      }
+    }
+  }
+  return result;
+};
 
 module.exports = isUppercase;
