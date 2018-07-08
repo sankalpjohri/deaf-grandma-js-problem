de8a8aedb277628ded4374298e89589e77cddc28
Sankalp Johri <sankalp.johri@iiitb.org>
2018-07-08 21:32:47 +0530

    Added method for conversation

--- index.js
+++ index.js
@@ -13,3 +13,24 @@ const { prompt, print } = require('./prompt-print');
  * 1. prompt function to get input from the user
  * 2. print function to print text on to the console
  */
+
+function grandmaConversation() {
+  print("What do you want to say to Grandma!!");
+  var byeCount = 0;
+  while (true) {
+    var input = prompt;
+    if (byeCount >= 3) {
+      break;
+    }
+    if (!isUppercase(input)) {
+      print("HUH?! SPEAK UP, SONNY!");
+    } else {
+      if (input == "BYE") {
+        byeCount++;
+      } else {
+        var response = "NO, NOT SINCE " + getRandomValue(1930, 1950);
+        print(response);
+      }
+    }
+  }
+}
